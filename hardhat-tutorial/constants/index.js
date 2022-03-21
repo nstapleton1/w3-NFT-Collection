17c9d1a47c8fb00f3d44984fda26febe86beed30
Niall Stapleton <niall.stapleton@gmail.com>
2022-03-21 17:09:28 +0000

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x96434d556Af7f03Ab4cA99529F466cf5B486a574";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
